2513e31991f8c86ba62ec22fccd71cdbb1f2144d
Kishan Tarapara <31764032+kishantarapara@users.noreply.github.com>
2023-07-30 14:42:14 +0530

    Update image-comparision-block.php

--- image-comparision-block.php
+++ image-comparision-block.php
@@ -9,7 +9,7 @@ function image_comparison_block_register_block() {
 
     // Register JavaScript File
     wp_register_script(
-        'custom-block1',
+        'image-comparision-block',
         plugins_url('build/index.js', __FILE__),
         array( 'wp-blocks', 'wp-element', 'wp-editor', 'wp-block-editor', 'wp-components' ),
         filemtime( plugin_dir_path( __FILE__ ) . 'build/index.js' )
@@ -17,7 +17,7 @@ function image_comparison_block_register_block() {
     
     // Register JavaScript File
     wp_register_script(
-        'custom-block2',
+        'image-comparision-block',
         plugins_url('src/view.js', __FILE__),
         array( 'wp-blocks', 'wp-element', 'wp-editor', 'wp-block-editor', 'wp-components' ),
         filemtime( plugin_dir_path( __FILE__ ) . 'src/view.js' )
@@ -25,7 +25,7 @@ function image_comparison_block_register_block() {
 
     // Register Editor Style
     wp_register_style(
-        'custom-block-editor-style1',
+        'image-comparision-block-editor-style',
         plugins_url('src/editor.css', __FILE__),
         array( 'wp-edit-blocks' ),
         filemtime( plugin_dir_path( __FILE__ ) . 'src/editor.css' )
@@ -33,7 +33,7 @@ function image_comparison_block_register_block() {
 
     // Register Front End Block Style
     wp_register_style(
-        'custom-block-frontend-style1',
+        'image-comparision-block-frontend-style',
         plugins_url('src/style.css', __FILE__),
         array( ),
         filemtime( plugin_dir_path( __FILE__ ) . 'src/style.css' )
@@ -41,10 +41,10 @@ function image_comparison_block_register_block() {
 
     // Register Block
     register_block_type('image-comparison-block/interactive-image-comparison-block', array(
-        'editor_script' => 'custom-block1',
-        'editor_style' =>  'custom-block-editor-style1',
-        'style' => 'custom-block-frontend-style1',
-        'script' => 'custom-block2',
+        'editor_script' => 'image-comparision-block1',
+        'editor_style' =>  'image-comparision-block-editor-style1',
+        'style' => 'image-comparision-block-frontend-style1',
+        'script' => 'image-comparision-block2',
     ));
 }
 
